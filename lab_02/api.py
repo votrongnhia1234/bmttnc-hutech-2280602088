183d12d7bddfec3f35bcd8bea6713c5516d8dabd
Võ Trọng Nghĩa <vtrongnghia04@gmail.com>
2025-06-11 13:32:30 +0700

    add aes_rsa_socket

--- lab_02/api.py
+++ lab_02/api.py
@@ -38,7 +38,7 @@ def vigenere_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
     key = data['key']
-    decrypted_text = vigenere_cipher.virgenere_decrypt(cipher_text, key)
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
 #RAILFENCE CIPHER ALGORITHM
